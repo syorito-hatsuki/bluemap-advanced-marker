5be688164d4921cf791fd167fcc22b693b04e816
syorito-hatsuki <enderdron3@gmail.com>
2022-08-25 15:18:47 +0300

    Some more no work sketches

--- src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/command/LineMarkerCommand.kt
+++ src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/command/LineMarkerCommand.kt
@@ -4,6 +4,8 @@ import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.StringArgumentType
 import dev.syoritohatsuki.bluemapadvancedmarker.BlueMapAdvancedMarkerAddon
 import dev.syoritohatsuki.bluemapadvancedmarker.util.BlueMapHelper
+import dev.syoritohatsuki.bluemapadvancedmarker.util.toVector3d
+import net.minecraft.command.argument.ColorArgumentType
 import net.minecraft.command.argument.Vec3ArgumentType
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
@@ -13,18 +15,30 @@ object LineMarkerCommand {
         dispatcher.register(
             CommandManager.literal("marker").then(
                 CommandManager.literal("line").then(
-                    CommandManager.argument("pos1", Vec3ArgumentType.vec3()).then(
-                        CommandManager.argument("pos2", Vec3ArgumentType.vec3()).then(
-                            CommandManager.argument("name", StringArgumentType.string()).executes {
-                                BlueMapAdvancedMarkerAddon.logger.info("Execute start")
-                                val player = it.source.player
+                    CommandManager.argument("name", StringArgumentType.string()).then(
+                        CommandManager.argument("color", ColorArgumentType.color()).then(
+                            CommandManager.argument("pos1", Vec3ArgumentType.vec3()).then(
+                                CommandManager.argument("pos2", Vec3ArgumentType.vec3()).executes {
+                                    BlueMapAdvancedMarkerAddon.logger.info("Execute start")
+                                    val player = it.source.player
+                                    val pos1 = Vec3ArgumentType.getVec3(it, "pos1").toVector3d()
+                                    val pos2 = Vec3ArgumentType.getVec3(it, "pos1").toVector3d()
 
-                                if (player != null) BlueMapHelper.createLine(player)
-                                else BlueMapAdvancedMarkerAddon.logger.info("Player not found")
+                                    if (player != null) {
+                                        BlueMapHelper.createLine(
+                                            StringArgumentType.getString(it, "name"),
+                                            ColorArgumentType.getColor(it, "color").colorValue!!,
+                                            it.source.world,
+                                            player,
+                                            pos1,
+                                            pos2
+                                        )
+                                    } else BlueMapAdvancedMarkerAddon.logger.info("Player not found")
 
-                                BlueMapAdvancedMarkerAddon.logger.info("Execute end")
-                                return@executes 1
-                            }
+                                    BlueMapAdvancedMarkerAddon.logger.info("Execute end")
+                                    return@executes 1
+                                }
+                            )
                         )
                     )
                 )
--- src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/util/BlueMapHelper.kt
+++ src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/util/BlueMapHelper.kt
@@ -1,17 +1,71 @@
 package dev.syoritohatsuki.bluemapadvancedmarker.util
 
+import com.flowpowered.math.vector.Vector3d
 import de.bluecolored.bluemap.api.BlueMapAPI
+import de.bluecolored.bluemap.api.markers.LineMarker
+import de.bluecolored.bluemap.api.markers.MarkerSet
+import de.bluecolored.bluemap.api.math.Color
+import de.bluecolored.bluemap.api.math.Line
+import dev.syoritohatsuki.bluemapadvancedmarker.BlueMapAdvancedMarkerAddon
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.world.World
+import java.util.function.Consumer
 
 object BlueMapHelper {
     private val blueMapAPI = BlueMapAPI.getInstance().get()
 
-    fun createLine(playerEntity: PlayerEntity) {
-        val world = blueMapAPI.getWorld(playerEntity.world).get().maps.toList()[0]
-        TODO("Added marker adding")
+    fun createLine(title: String, color: Int, world: World, playerEntity: PlayerEntity, vararg positions: Vector3d) {
+        BlueMapAdvancedMarkerAddon.logger.info("=======================")
+        val playerUUID = playerEntity.uuid.toString()
+        BlueMapAdvancedMarkerAddon.logger.info(playerUUID)
+        blueMapAPI.getWorld(world).ifPresentOrElse({
+            BlueMapAdvancedMarkerAddon.logger.info(it.toString())
+
+            val markerSet = MarkerSet.builder().label(playerUUID).build()
+            BlueMapAdvancedMarkerAddon.logger.info(markerSet.toString())
+
+            val marker = LineMarker.builder()
+                .label(title)
+                .line(Line(*positions))
+                .lineColor(Color(color))
+                .depthTestEnabled(true)
+                .build()
+            BlueMapAdvancedMarkerAddon.logger.info(marker.toString())
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+
+
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+            BlueMapAdvancedMarkerAddon.logger.info(playerUUID)
+            BlueMapAdvancedMarkerAddon.logger.info(it.toString())
+            BlueMapAdvancedMarkerAddon.logger.info(markerSet.toString())
+            BlueMapAdvancedMarkerAddon.logger.info(marker.toString())
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+
+            markerSet.markers["$playerUUID/$title"] = marker
+
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+            BlueMapAdvancedMarkerAddon.logger.info(playerUUID)
+            BlueMapAdvancedMarkerAddon.logger.info(it.toString())
+            BlueMapAdvancedMarkerAddon.logger.info(markerSet.toString())
+            BlueMapAdvancedMarkerAddon.logger.info(marker.toString())
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+
+            it.maps.toList()[0].markerSets[playerUUID] = markerSet
+
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+            BlueMapAdvancedMarkerAddon.logger.info(playerUUID)
+            BlueMapAdvancedMarkerAddon.logger.info(it.toString())
+            BlueMapAdvancedMarkerAddon.logger.info(markerSet.toString())
+            BlueMapAdvancedMarkerAddon.logger.info(marker.toString())
+            BlueMapAdvancedMarkerAddon.logger.info("=======================")
+
+        }, {
+            BlueMapAdvancedMarkerAddon.logger.info("ELSE")
+        })
+
     }
 
     fun removeLine() {
-        TODO("Added marker removing")
+
     }
 }
