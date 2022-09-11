77478fe0bba99ca32e15e3d52f1c2a894e37a882
Syorito Hatsuki <33298273+syorito-hatsuki@users.noreply.github.com>
2022-09-11 05:13:04 +0300

    Delete src/main directory

--- src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/util/Extension.kt
+++
@@ -1,6 +0,0 @@
-package dev.syoritohatsuki.bluemapadvancedmarker.util
-
-import com.flowpowered.math.vector.Vector3d
-import net.minecraft.util.math.Vec3d
-
-fun Vec3d.toVector3d() = Vector3d(this.x, this.y, this.z)
