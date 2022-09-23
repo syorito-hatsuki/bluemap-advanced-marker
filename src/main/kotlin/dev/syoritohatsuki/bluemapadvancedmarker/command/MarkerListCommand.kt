7450160e12761038e42bd2c0b095aab597d594f9
syorito-hatsuki <enderdron3@gmail.com>
2022-09-23 23:13:27 +0300

    specify player marker list

--- src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/command/MarkerListCommand.kt
+++ src/main/kotlin/dev/syoritohatsuki/bluemapadvancedmarker/command/MarkerListCommand.kt
@@ -2,12 +2,14 @@ package dev.syoritohatsuki.bluemapadvancedmarker.command
 
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
 import de.bluecolored.bluemap.api.BlueMapAPI
 import dev.syoritohatsuki.bluemapadvancedmarker.BlueMapAdvancedMarkerAddon.MOD_ID
 import dev.syoritohatsuki.bluemapadvancedmarker.manager.PlayerCacheManager
 import dev.syoritohatsuki.bluemapadvancedmarker.registry.CommandRegistry.commandLiteral
 import me.lucko.fabric.api.permissions.v0.Permissions
+import net.minecraft.command.argument.EntityArgumentType
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.text.Text
@@ -19,7 +21,13 @@ object MarkerListCommand {
             CommandManager.literal(commandLiteral).then(
                 CommandManager.literal("list").requires(
                     Permissions.require("$MOD_ID.list", 0)
-                ).executes { executeOwnMarkers(it) }
+                ).executes { executeOwnMarkers(it) }.then(
+                    CommandManager.argument("playerName", StringArgumentType.word())
+                        .requires(Permissions.require("$MOD_ID.list.other", 2))
+                        .executes { executeListUserPoint(it, false) }).then(
+                    CommandManager.argument("playerEntity", EntityArgumentType.player())
+                        .requires(Permissions.require("$MOD_ID.list.other", 2))
+                        .executes { executeListUserPoint(it, true) })
             ).then(
                 CommandManager.literal("listall").requires(
                     Permissions.require("$MOD_ID.listall", 2)
@@ -30,7 +38,7 @@ object MarkerListCommand {
 
     private fun executeAllUsersMarkers(context: CommandContext<ServerCommandSource>): Int {
         context.source.sendFeedback(Text.of(StringBuilder().apply {
-            append("\n=======[ All marker's ]=======")
+            append("\n=======[ All marker's ]=======\n")
             BlueMapAPI.getInstance().get().maps.forEach { map ->
                 append("\n  ${map.name}")
                 map.markerSets.forEach { (name, value) ->
@@ -38,6 +46,7 @@ object MarkerListCommand {
                         append("\n      Map: ${map.name} Owner: ${PlayerCacheManager.getPlayer(UUID.fromString(name)).name} Name: ${it.label}")
                     }
                 }
+                append("\n")
             }
         }.toString()), false)
         return Command.SINGLE_SUCCESS
@@ -52,6 +61,27 @@ object MarkerListCommand {
                     .forEach { (_, value) ->
                         value.markers.values.forEach { append("\n      Map: ${map.name} Name: ${it.label}") }
                     }
+                append("\n")
+            }
+        }.toString()), false)
+        return Command.SINGLE_SUCCESS
+    }
+
+    private fun executeListUserPoint(context: CommandContext<ServerCommandSource>, entity: Boolean): Int {
+        val uuid = when {
+            entity -> EntityArgumentType.getPlayer(context, "playerEntity").uuidAsString
+            else -> PlayerCacheManager.getPlayer(StringArgumentType.getString(context, "playerName")).uuid
+        }
+
+        context.source.sendFeedback(Text.of(StringBuilder().apply {
+            append("\n=======[ ${PlayerCacheManager.getPlayer(uuid).name} marker's ]=======\n")
+            BlueMapAPI.getInstance().get().maps.forEach { map ->
+                append("\n  ${map.name}")
+                map.markerSets.filter { it.key.contains(uuid) }
+                    .forEach { (_, value) ->
+                        value.markers.values.forEach { append("\n      Map: ${map.name} Name: ${it.label}") }
+                    }
+                append("\n")
             }
         }.toString()), false)
         return Command.SINGLE_SUCCESS
